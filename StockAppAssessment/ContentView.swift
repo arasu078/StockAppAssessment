819ff4eee7411f8159c0bfddaf241a41431bdbb7
Ponnarasu Murugesan <ponnarasu.it@gmail.com>
2026-04-05 18:55:16 +0400

    Initial Commit

---
+++ StockAppAssessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockAppAssessment
+//
+//  Created by Ponnarasu on 05/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockAppAssessment/StockAppAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppAssessmentApp.swift
+//  StockAppAssessment
+//
+//  Created by Ponnarasu on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockAppAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
